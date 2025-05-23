4edb53bf5fbfa1decc7528e6e985f1376880503a
pichuck <kadavi9945@gmail.com>
2025-05-23 21:54:07 +0700

    Initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Login" />
+
+    <div class="flex justify-center items-center h-screen">
+        <h1 class="text-center text-xl font-bold">
+            Testing Ini berhasil setup
+        </h1>
+    </div>
+</template>
