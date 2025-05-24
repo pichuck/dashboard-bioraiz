817a476f90ad127095deb70af721cedeed299dd5
pichuck <kadavi9945@gmail.com>
2025-05-24 22:29:12 +0700

    feat : Frontend Login

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Login" />
+
+    <section class="page-section login-page">
+        <div class="full-width-screen">
+            <div class="container-fluid">
+                <div class="content-detail">
+                    <div class="main-info">
+                        <div class="hero-container">
+                            <!-- Login form -->
+                            <form class="login-form" method="post">
+                                <div
+                                    class="imgcontainer flex justify-center items-center"
+                                >
+                                    <img
+                                        src="/assets/images/bioraiz-text.png"
+                                        style="width: 180px"
+                                        alt="Avatar"
+                                        class="avatar"
+                                    />
+                                </div>
+                                <div class="input-control">
+                                    <input
+                                        type="text"
+                                        placeholder="Enter Username"
+                                        name="uname"
+                                        required
+                                    />
+                                    <span class="password-field-show">
+                                        <input
+                                            type="password"
+                                            placeholder="Enter Password"
+                                            name="password"
+                                            class="password-field"
+                                            value=""
+                                            required
+                                        />
+                                    </span>
+                                    <label class="label-container"
+                                        >Remember me
+                                        <input type="checkbox" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <span class="psw"
+                                        ><a
+                                            href="forgot.html"
+                                            class="forgot-btn"
+                                            >Forgot password?</a
+                                        ></span
+                                    >
+                                    <div class="login-btns">
+                                        <button type="submit">Login</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+         <!-- Bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
+    <!-- Theme css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/login.css">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
